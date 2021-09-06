7b58856c5ff5eca65b7475c6c76788f2d4f15cf0
junemp <junemp@gmail.com>
2021-09-06 13:49:31 +0900

    상품 상세 타이틀 수정

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -27,15 +27,17 @@ export default {
     return { product }
   },
 
-  head: {
-    title: 'Shopping Item Detail',
-    meta: [
-      {
-        hid: 'description',
-        name: 'description',
-        content: '이 상품은 ~~ 입니다.',
-      },
-    ],
+  head() {
+    return {
+      title: `Shopping Item Detail - ${this.product.name}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: `이 상품은 ${this.product.name} 입니다.`,
+        },
+      ],
+    }
   },
 
   methods: {
